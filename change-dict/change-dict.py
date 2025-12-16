6b05f7e7ac5af3e5f8e64b42712fe26797f404ba
Proodle0911 <pranav.parab.dev@gmail.com>
2025-12-16 19:11:57 +0000

    feat: Add all course exercises

--- change-dict/change-dict.py
+++ change-dict/change-dict.py
@@ -2,3 +2,18 @@
 x = {'type' : 'fruit', 'name' : 'banana'}
 # What is a correct syntax for changing the type from fruit to berry?
 x['type'] = 'berry'
+
+
+# Change the "year" value from 1964 to 2020.
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"]=2020 
+
+
+# Consider the following code:
+x = {'type' : 'fruit', 'name' : 'banana'}
+# What is a correct syntax for changing the name from banana to apple?
+x.update({'name': 'apple'})
